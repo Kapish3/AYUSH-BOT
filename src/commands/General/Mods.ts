f59cb1c030c18efd7d5305ff6287cf618071229f
AYUSH/LUCIFER <96679851+monarch21@users.noreply.github.com>
2022-02-05 19:32:57 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
             (user, index) =>
                 (text += `#${index + 1}\n🌟 *Username: ${
                     user.notify || user.vname || user.name || 'null'
-                }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
+                }*\n🍀 *Contact: https://wa.me/+${user?.jid.split('@')[0]}*\n\n`)
         )
         text += `\n🌟 Chitoge 🌟 `
         return void M.reply(text)
