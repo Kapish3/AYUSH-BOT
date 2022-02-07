944b519b5fbabb4fa9febb4fa6d49fdd479945a0
AYUSH/LUCIFER <96679851+monarch21@users.noreply.github.com>
2022-02-08 03:43:21 +0530

    Update bp.ts

--- src/commands/creations/bp.ts
+++ src/commands/creations/bp.ts
@@ -23,7 +23,7 @@ export default class Command extends BaseCommand {
 	run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
 		 if (!joined) return void (await M.reply(`Provide the text, Baka!`))
         const cara = joined.trim()
-		const wall = await w5botapi.textpro("https://textpro.me/create-pink-blackpinklogo-text-effects-online-1027.html",
+		const wall = await w5botapi.textpro("https://textpro.me/create-pink-blackpink-logo-text-effects-online-1027.html",
     cara
     );
 		const buffer = await request.buffer(wall).catch((e) => {
